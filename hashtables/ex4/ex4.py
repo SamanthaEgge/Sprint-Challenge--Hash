294d12b088060a6b61606326038a866284f0f404
Samantha Egge <samantha.c.egge@gmail.com>
2020-05-08 08:43:42 -0700

    Initial code written for negative check in example 4. Running tests now.

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,25 @@
 def has_negatives(a):
+  #initializing some shizzz
+  result = []
+  numb_dict = {}
 
-    """
-    YOUR CODE HERE
-    """
+  ### numbers will be in random order, so -1 number might come before a positive one
+  ## Do a check of each number??
+  ## create a dict with the inverse value
+  ## if numb_dict[key] exists as a key, add to result?
 
-    return result
+  ## Generate dict
+  for number in a:
+    numb_dict[number] = -number
+
+  ## Now to check 
+  for key in numb_dict:
+    if key < 0:
+      if numb_dict[-key]:
+        result.append(key)
+ 
+  return result
 
 
 if __name__ == "__main__":
-    print(has_negatives([-1,-2,1,2,3,4,-4]))
+  print(has_negatives([-1,-2,1,2,3,4,-4]))
