b08666dd4010133f1dc3fc5d7633bc9e3b2f5097
Samantha Egge <samantha.c.egge@gmail.com>
2020-05-08 10:15:44 -0700

    Brute force solution for example 5, querying files, is complete. Need to check tests, and then refactor.

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,21 +1,37 @@
 def finder(files, queries):
+  result = []
+  file_dict = {}
 
-    """
-    YOUR CODE HERE
-    """
+  ## create a dictionary
+  # what should be my key: value pairs?
+  # save whole path as key?
+  ## use rsplit('/', 1) to check query against value
 
-    return result
+  for file in files:
+    ## this is taking off the last bit of the file name to save as value
+    file_split = file.rsplit('/', 1)
+    file_dict[file] = file_split[1]
+
+  ### Need to check for each key in file_dict if the value = queries
+  ### Brute force is going to be A BIG BITCH
+  for file in file_dict:
+    print(file)
+    for check in queries:
+      if file_dict[file] == check:
+        result.append(file)
+
+  return result
 
 
-if __name__ == "__main__":
-    files = [
-        '/bin/foo',
-        '/bin/bar',
-        '/usr/bin/baz'
-    ]
-    queries = [
-        "foo",
-        "qux",
-        "baz"
-    ]
-    print(finder(files, queries))
+# if __name__ == "__main__":
+#   files = [
+#     '/bin/foo',
+#     '/bin/bar',
+#     '/usr/bin/baz'
+#   ]
+#   queries = [
+#     "foo",
+#     "qux",
+#     "baz"
+#   ]
+#   print(finder(files, queries))
