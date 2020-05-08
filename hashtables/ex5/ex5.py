d601b6b4b249de12b3b802d85c7637cb6c295422
Samantha Egge <samantha.c.egge@gmail.com>
2020-05-08 11:00:59 -0700

    Further work on the ex5 problem. More pseudocode. Times up, pushing to master.

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -32,16 +32,16 @@ class LinkedDictNode:
 
   def insert_after(self, value):
     current_next = self.next
-    self.next = ListNode(value, self, current_next)
+    self.next = LinkedDictNode(value, self, current_next)
     if current_next:
       current_next.prev = self.next
-​
+
   def insert_before(self, value):
     current_prev = self.prev
-    self.prev = ListNode(value, current_prev, self)
+    self.prev = LinkedDictNode(value, current_prev, self)
     if current_prev:
       current_prev.next = self.prev
-​
+
   def delete(self):
     if self.prev:
       self.prev.next = self.next
@@ -53,12 +53,12 @@ class DoublyLinkedDict:
     self.head = node
     self.tail = node
     self.length = 1 if node is not None else 0
-​
+
   def __len__(self):
     return self.length
-​
+
   def add_to_head(self, value):
-    new_node = ListNode(value)
+    new_node = LinkedDictNode(value)
     self.length += 1
     if not self.head and not self.tail:
       self.head = new_node
@@ -67,9 +67,9 @@ class DoublyLinkedDict:
       new_node.next = self.head
       self.head.prev = new_node
       self.head = new_node
-​
+
   def add_to_tail(self, value):
-    new_node = ListNode(value)
+    new_node = LinkedDictNode(value)
     self.length += 1
     if not self.head and not self.tail:
       self.head = new_node
@@ -83,25 +83,35 @@ def finder(files, queries):
   result = []
   file_dict = {}
 
-  ## create a dictionary
-  # what should be my key: value pairs?
-  # save whole path as key?
-  ## use rsplit('/', 1) to check query against value
-
+  ## create a dictionary of the files
+  # since we're searching for the file name only, save file name as key
+  # in linked list, save each full file directory to the dictionary
+  # Going to use rsplit (right split) to get the file name
   for file in files:
-    ## this is taking off the last bit of the file name to save as value
+    #this produces foo, bar, baz
     file_split = file.rsplit('/', 1)
-    file_dict[file] = file_split[1]
+    ## this is checking if file name already exists in dictionary
+    if file_split[1] in file_dict:
+      print(file_dict[file_split[1]])
+      file_dict[file_split[1]].add_to_tail(file)
+    else:
+      ## create new linked list, starting with this filepath
+      file_dict[file_split[1]] = DoublyLinkedDict(file)
+  
+  print(file_dict)
 
-  ### Need to check for each key in file_dict if the value = queries
-  ### Brute force is going to be A BIG BITCH
-  for file in file_dict:
-    # print(file)
-    for check in queries:
-      if file_dict[file] == check:
-        result.append(file)
+  ## For each query, check to see if that file exists.
+  # if so, append each filename to the result
+
+  for query in queries:
+    if query in file_dict:
+      
 
   return result
+
+
+########## TODO:Current is having issues with adding new nodes to list
+
 
 # if __name__ == "__main__":
 #   files = [
