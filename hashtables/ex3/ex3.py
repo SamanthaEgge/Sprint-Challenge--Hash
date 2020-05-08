965194f13fef7f386a9158d3eb52a90ad6186ca9
Samantha Egge <samantha.c.egge@gmail.com>
2020-05-08 08:10:09 -0700

    Started interesection hash problem, #3. Set up most of the code, and in theory it's working. Need to figure out how to access the key.

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,17 +1,33 @@
 def intersection(arrays):
+  result = []
+  ## get numbah of arrays
+  numbArrays = len(arrays)
+  #init array dictionary
+  arrayDict = {}
 
-    """
-    YOUR CODE HERE
-    """
+  ## create dictionary based upon array input
+  ## if key is found, increment value by 1
+  ## if key = len(arrays), add to result
+  ## profit?
 
-    return result
+  for smolArray in arrays:
+    for number in smolArray:
+      if number not in arrayDict:
+        arrayDict[number] = 1
+      arrayDict[number] += 1
+      if arrayDict[number] == numbArrays:
+        ### how do i get the KEEEEY
+        print('this passed')
+
+
+  return result
 
 
 if __name__ == "__main__":
-    arrays = []
+  arrays = []
 
-    arrays.append(list(range(1000000,2000000)) + [1,2,3])
-    arrays.append(list(range(2000000,3000000)) + [1,2,3])
-    arrays.append(list(range(3000000,4000000)) + [1,2,3])
+  arrays.append(list(range(1000000,2000000)) + [1,2,3])
+  arrays.append(list(range(2000000,3000000)) + [1,2,3])
+  arrays.append(list(range(3000000,4000000)) + [1,2,3])
 
-    print(intersection(arrays))
+  print(intersection(arrays))
