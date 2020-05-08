37914b9622bd908f9a9985b48e2dead5e0ce499a
Samantha Egge <samantha.c.egge@gmail.com>
2020-05-08 08:19:58 -0700

    Fixed issue with ex3, interesections, file. In my logic to find the interesections I was accidentally counting +1 instead of starting with a count of 1 when initialized. Tests passing for ex3.

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,6 +1,7 @@
 def intersection(arrays):
   result = []
   ## get numbah of arrays
+  print(len(arrays))
   numbArrays = len(arrays)
   #init array dictionary
   arrayDict = {}
@@ -9,27 +10,28 @@ def intersection(arrays):
   ## if key is found, increment value by 1
   ## if key = len(arrays), add to result
   ## profit?
-
+  # print('we in intersection')
+  # print(arrays)
   for smolArray in arrays:
     for number in smolArray:
       if number not in arrayDict:
-        arrayDict[number] = 1
+        arrayDict[number] = 0
       arrayDict[number] += 1
+      # print('arrayDict')
+      # print(arrayDict)
       if arrayDict[number] == numbArrays:
         ### how do i get the KEEEEY --- I am not a smart lass
         result.append(number)
         # print(number)
-        # print('this passed')
-
-
+  print(result)
   return result
 
 
-if __name__ == "__main__":
-  arrays = []
+# if __name__ == "__main__":
+#   arrays = []
 
-  arrays.append(list(range(1000000,2000000)) + [1,2,3])
-  arrays.append(list(range(2000000,3000000)) + [1,2,3])
-  arrays.append(list(range(3000000,4000000)) + [1,2,3])
+#   arrays.append(list(range(1000000,2000000)) + [1,2,3])
+#   arrays.append(list(range(2000000,3000000)) + [1,2,3])
+#   arrays.append(list(range(3000000,4000000)) + [1,2,3])
 
-  print(intersection(arrays))
+#   print(intersection(arrays))
